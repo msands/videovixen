a8dfe6d15c16ba1d786985d19128ffbba873e613
Marcel Sands <marcel.sands84@gmail.com>
2016-04-13 12:15:48 -0400

    install devise and add static pages controller [start #117409977]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  get 'about' => 'static_pages#about'
+  get 'terms' => 'static_pages#terms'
+  get 'privacy' => 'static_pages#privacy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
