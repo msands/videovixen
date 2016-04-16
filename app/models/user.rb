67ebe5e3bbd0b1b7724c8186e3d1c5c779dd7a8b
Marcel Sands <marcel.sands84@gmail.com>
2016-04-16 01:14:36 -0400

    user can log in using email or username [finishes #117746505]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,51 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+  # after_initialize :create_login, :if => :new_record?
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value or lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  # def create_login
+  #   if self.username.blank?
+  #     email = self.email.split(/@/)
+  #     login_taken = Pro.where(:username => email[0]).first
+  #     unless login_taken
+  #       self.username = email[0]
+  #     else
+  #       self.username = self.email
+  #     end
+  #   end
+  # end
+
 end
