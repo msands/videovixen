b245f94ab46bee70f92855849140edfcbc28c0b9
Marcel Sands <marcel.sands84@gmail.com>
2016-05-07 21:15:56 -0400

    update vixen profile index page and css

--- app/controllers/vixen_profiles_controller.rb
+++ app/controllers/vixen_profiles_controller.rb
@@ -1,5 +1,5 @@
 class VixenProfilesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   skip_before_action :check_profile, only: [:new, :create]
 
   def index
