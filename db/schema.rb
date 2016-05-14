1be5d0546d537639a3f090df78cc803d7517905c
Marcel Sands <marcel.sands84@gmail.com>
2016-05-14 18:40:28 -0400

    add facial fields to talent profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513141702) do
+ActiveRecord::Schema.define(version: 20160514222119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 20160513141702) do
     t.integer  "glove"
     t.integer  "hat"
     t.integer  "bicep"
+    t.string   "eye_color"
+    t.string   "hair_color"
+    t.string   "hair_length"
+    t.string   "hair_type"
+    t.string   "skills"
+    t.string   "languages",                             array: true
   end
 
   create_table "users", force: :cascade do |t|
