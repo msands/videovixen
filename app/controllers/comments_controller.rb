fcca536f87e501783365b374916d03a0098b5f68
Marcel Sands <marcel.sands84@gmail.com>
2016-05-19 23:06:37 -0400

    add logic to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,21 @@
 class CommentsController < ApplicationController
+  before_filter :load_commentable
+
   def index
+    @commentable = @commentable.comments
   end
 
   def new
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def load_commentable
+    resource, id = request.path.split('/')[1,2]
+    @commentable = resource.singularize.classify.constantize.find(id)
   end
 end
